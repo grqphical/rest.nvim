0660b7f248e7bf5f8cc1d19de0e8917bf910e650
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2025-08-07 15:45:52 -0700

    feat: ✨ added prefix highlights

--- lua/rest/highlights.lua
+++ lua/rest/highlights.lua
@@ -1,7 +1,8 @@
 local M = {}
 
 M.apply_request_highlights = function()
-
+    vim.fn.matchadd("RestRequestPrefix", [[/\w\+:/]])
+    vim.api.nvim_set_hl(0, "RestRequestPrefix", { guifg = "#00ccff" })
 end
 
 M.apply_response_highlights = function()
