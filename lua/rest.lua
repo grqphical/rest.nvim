093c5f31f75ab8b956d5d7a0b243b00fbf0172e7
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2025-08-07 10:22:29 -0700

    feat: ✨ parsed lines are converted to request

--- lua/rest.lua
+++ lua/rest.lua
@@ -11,12 +11,12 @@ local function split(s, delimiter)
     return t
 end
 
---@class rest.Request
---@field url string URL to send the request to
---@field method string HTTP method to use
---@field version string HTTP version to use
---@field header table<string, string> HTTP header
---@field body string The HTTP Request Body
+---@class rest.Request
+---@field url string URL to send the request to
+---@field method string HTTP method to use
+---@field version string HTTP version to use
+---@field header table<string, string> HTTP header
+---@field body string The HTTP Request Body
 
 local function parse_line(line)
     local result = {
@@ -30,7 +30,7 @@ local function parse_line(line)
         if char == ":" then
             parsingKey = false
 
-            -- remove space after colon if it exists
+            --- remove space after colon if it exists
             if #line >= i + 1 and line:sub(i + 1, i + 1) == " " then
                 line = line:sub(1, i - 1) .. line:sub(i + 1)
             end
@@ -48,19 +48,39 @@ local function parse_line(line)
     return result
 end
 
---@param contents string
---@return rest.Request
-local function parse_rest_buffer(contents)
-    for i, line in ipairs(split(contents, "\n")) do
+---@param contents string
+---@return rest.Request
+M.__parse_rest_buffer = function(contents)
+    local request = {
+        method = "GET",
+        header = {
+            ["User-Agent"] = "rest.nvim/0.1.0 curl/7.54.1"
+        },
+        version = "HTTP/1.1",
+        body = "",
+        url = ""
+    }
+
+    for line in split(contents, "\n") do
         local result = parse_line(line)
-        print(result.key, result.value)
+        if result.key == "method" then
+            request.method = result.value
+        elseif result.key == "url" then
+            request.url = result.value
+        elseif result.key == "header" then
+            request.header[result.key] = result.value
+        elseif result.key == "version" then
+            request.version = result.value
+        elseif result.key == "body" then
+            request.body = result.value
+        end
     end
+
+    return request
 end
 
 M.setup = function(config)
     print("Loaded rest.nvim")
-
-    parse_rest_buffer("url: https://example.com\nheader: Content-Type: application/json\nbody: {\"foo\":\"bar\"}")
 end
 
 return M
