3832a540bdea78415f93f04212d9fd082b51ee64
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2025-08-07 19:45:51 -0700

    feat: ✨ added more highlights for request buffers

--- plugin/load_rest.lua
+++ plugin/load_rest.lua
@@ -14,8 +14,27 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = "rest.nvim-request",
     callback = function()
         vim.cmd [[
-            syn match restTag /^\s*[^:]*:\ze/
+            syn match restTag /^\s*\zs[^:]\+/
+
+            syn keyword restMethod GET
+            syn keyword restMethod POST
+            syn keyword restMethod PATCH
+            syn keyword restMethod PUT
+            syn keyword restMethod DELETE
+            syn keyword restMethod INFO
+            syn keyword restMethod HEAD
+            syn keyword restMethod OPTIONS
+            syn keyword restMethod CONNECT
+            syn keyword restMethod TRACE
+
+            syn match restVersion /HTTP\/1.0/
+            syn match restVersion /HTTP\/1.1/
+            syn match restVersion /HTTP\/2/
+            syn match restVersion /HTTP\/3/
+
             hi def link restTag Statement
+            hi def link restMethod Constant
+            hi def link restVersion Constant
         ]]
     end
 })
